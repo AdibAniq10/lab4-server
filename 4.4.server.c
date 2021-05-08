9778013b6a21a6140033f44d6bf755aeddbb7bc5
AdibAniq10 <adibaniq246@gmail.com>
2021-05-08 23:23:23 +0000

    commit for lab4-server

---
+++ 4.4.server.c
@@ -0,0 +1,95 @@
+#include <stdio.h>
+#include <netdb.h>
+#include <netinet/in.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+#define MAX 2000
+#define PORT 7000
+#define SA struct sockaddr
+
+void func(int sockfd)
+{
+    char buff[MAX];
+    int a;
+    // infinite loop for chat
+    for (;;) {
+        bzero(buff, MAX);
+
+        
+        read(sockfd, buff, sizeof(buff));
+        // print buffer which contains the client contents
+        printf("From Client user message is : %s\t for client user : ", buff);
+        bzero(buff, MAX);
+        a = 0;
+        // copy server message in the buffer
+        while ((buff[a++] = getchar()) != '\a')
+            ;
+
+        // and send that buffer to client
+        write(sockfd, buff, sizeof(buff));
+
+        // if msg contains "Exit" then server exit and chat ended.
+        if (strncmp("exit", buff, 4) == 0) {
+            printf("Server is closing....\n");
+            break;
+        }
+    }
+}
+
+// Driver function
+int main()
+{
+    int sockfd, connfd, len;
+    struct sockaddr_in servaddr, cli;
+
+    // socket create and verification
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd == -1) {
+        printf("There error in socket creation...\n");
+        exit(0);
+    }
+    else
+        printf("Socket successfully created..\n");
+    bzero(&servaddr, sizeof(servaddr));
+
+    // assign IP, PORT
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+    servaddr.sin_port = htons(PORT);
+
+    // Binding newly created socket to given IP and verification
+    if ((bind(sockfd, (SA*)&servaddr, sizeof(servaddr))) != 0) {
+        printf("socket bind has error in it...\n");
+        exit(0);
+    }
+    else
+        printf("Socket successfully binded, congrats..\n");
+
+    // Now server is ready to listen and verification
+    if ((listen(sockfd, 5)) != 0) {
+        printf("There is error, Listen failed...\n");
+        exit(0);
+    }
+    else
+        printf("we are listening, Server..\n");
+    len = sizeof(cli);
+
+    // Accept the data packet from client and verification
+    connfd = accept(sockfd, (SA*)&cli, &len);
+    if (connfd < 0) {
+        printf("There is error, server acccept failed...\n");
+        exit(0);
+    }
+    else
+        printf("We received it...\n");
+
+    // Function for chatting between client and server
+    func(connfd);
+
+    // After chatting close the socket
+    close(sockfd);
+}
